32841ce0b4c7ffaded855f06075b4ce9fefd7dcb
Charlyboy1111 <desormeaux.charlesetienne@gmail.com>
2021-12-25 02:33:29 -0500

    Moved token into environment file

--- connections.py
+++ connections.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('TOKEN')
+
 socket = 'wss://ws.finnhub.io?token=c70t4t2ad3ifhkk9995g'
 
-token = 'UgYCvu4OUsWEnfzjmZxLOTVC4sR8nC95W4_45AwRRkJXq_Tt7lrNhNBR-pld3wO1DO1joy0_sTI_Na1atvTX3Q=='
+#token = 'UgYCvu4OUsWEnfzjmZxLOTVC4sR8nC95W4_45AwRRkJXq_Tt7lrNhNBR-pld3wO1DO1joy0_sTI_Na1atvTX3Q=='
 org = 'boga'
 bucket = 'temp'
