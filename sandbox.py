2186f08857eb6d9b3b7e674a72f07c643b1b0b1b
Wasim Boughattas <wassooma2013@yahoo.com>
2022-01-07 17:00:45 -0500

    keep one sandbox file

--- sandbox.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import finnhub
-
-from util.config import get_env
-
-if __name__ == '__main__':
-    get_env()
-    print(finnhub.Client(api_key=os.getenv('finnhub_token')).crypto_symbols('KUCOIN'))
