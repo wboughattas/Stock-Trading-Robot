7315c902efdb4846d88a84a6291bdcce5831c267
alessmorsella <alessandromorsella@gmail.com>
2022-01-09 16:44:47 -0500

    Collects every news article's title, content and publication date on Yahoo finance and performs Vader sentiment analysis on the content

--- news_headlines.py
+++ news_headlines.py
@@ -1,17 +1,33 @@
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from requests import get
 from urllib.request import urlopen
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import datetime
+import time
+from dateutil import parser
+import json
+import os
+from selenium.webdriver.chrome.service import Service
 
-driver=webdriver.Chrome('../../Desktop/Stock-Trading-Robot/chromedriver')
+
+nltk.downloader.download('vader_lexicon')
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("—disable-gpu")
+driver=webdriver.Chrome(options=chrome_options,executable_path='./chromedriver')
 driver.get("https://finance.yahoo.com/topic/crypto/")
-news_headlines=[]
-
+sentiment={}
+sentiment['crypto']=[]
+yahoo={}
+sentiment['crypto'].append(yahoo)
 while True:
     try:
         driver.find_element(By.XPATH, '//*[@id="Fin-Stream"]/ul/li[150]')
@@ -19,17 +35,21 @@ while True:
     except:
         driver.find_element(By.XPATH, '//*[@id="atomic"]/body').send_keys(Keys.END)
 for nb in range(1, 151):
-    news_headlines.append(driver.find_element(By.XPATH, '//*[@id="Fin-Stream"]/ul/li['+str(nb)+']/div/div/div[2]/h3/a').text)
-print(news_headlines)
+    article_content=''
+    unix_time=''
+    vader=SentimentIntensityAnalyzer()
+    title=driver.find_element(By.XPATH, '//*[@id="Fin-Stream"]/ul/li['+str(nb)+']/div/div/div[2]/h3/a').text
+    link=driver.find_element(By.XPATH, '//*[@id="Fin-Stream"]/ul/li[' + str(nb) + ']/div/div/div[2]/h3/a').get_attribute('href')
+    req=requests.get(link)
+    page=req.content
+    soup=BeautifulSoup(page,'html.parser')
+    for paragraphs in soup.find_all('div', class_="caas-body"):
+        article_content+=paragraphs.text
+    for d in soup.findAll('time'):
+        if d.has_attr('datetime'):
+            date_time=parser.parse(str(d['datetime']))
+            unix_time=date_time.timestamp()
+    score=vader.polarity_scores(article_content)
+    yahoo['article'+' - '+str(unix_time)]={'title':title, 'content':article_content,'time':unix_time, 'ntlk_sentiment':score}
 
-# BloombergBtc="https://www.bloomberg.com/search?query=bitcoin"
-# responseBloom=get(BloombergBtc)
-# soup2=BeautifulSoup(responseBloom.text,'html.parser')
-# for div in soup2.findAll('div', attrs={'class':'storyItem__'}):
-#     for bloomheadlines in div.find_all('a', attrs={'class':'headline__'}):
-#         new_headlines.append(bloomheadlines.text)
-#         print(bloomheadlines)
-
-
-
-
+print(yahoo)
